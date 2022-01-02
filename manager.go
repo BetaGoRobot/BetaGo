b7c2ddbf8dd434a55c5164a7a4002381999c5916
Kevin Matt <he_yuheng@163.com>
2022-01-02 14:45:51 +0800

    修复

--- manager.go
+++ manager.go
@@ -9,8 +9,8 @@ import (
 func replaceDirtyWords(ctx *khl.TextMessageContext) {
 	message := ctx.Common.Content
 	if strings.Contains(message, "傻") && strings.Contains(message, "逼") || strings.Contains(message, "傻逼") {
-		message = strings.ReplaceAll("傻", message, message)
-		message = strings.ReplaceAll("逼", message, message)
+		message = strings.ReplaceAll("傻", message, "")
+		message = strings.ReplaceAll("逼", message, "")
 		ctx.Session.MessageUpdate(&khl.MessageUpdate{MessageUpdateBase: khl.MessageUpdateBase{MsgID: ctx.Common.MsgID, Content: message}})
 		// ctx.Session.MessageCreate(&khl.MessageCreate{
 		// 	MessageCreateBase: khl.MessageCreateBase{
