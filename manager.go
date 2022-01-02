5a737d60f23c06004f14f23069908675823fcbf8
Kevin Matt <he_yuheng@163.com>
2022-01-02 15:04:08 +0800

    修正

--- manager.go
+++ manager.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/lonelyevil/khl"
@@ -9,17 +10,17 @@ import (
 func replaceDirtyWords(ctx *khl.TextMessageContext) {
 	message := ctx.Common.Content
 	if strings.Contains(message, "傻") && strings.Contains(message, "逼") || strings.Contains(message, "傻逼") {
-		message = strings.ReplaceAll("傻", message, "")
-		message = strings.ReplaceAll("逼", message, "")
-		ctx.Session.MessageUpdate(&khl.MessageUpdate{MessageUpdateBase: khl.MessageUpdateBase{MsgID: ctx.Common.MsgID, Content: message}})
-		// ctx.Session.MessageCreate(&khl.MessageCreate{
-		// 	MessageCreateBase: khl.MessageCreateBase{
-		// 		TargetID: ctx.Common.TargetID,
-		// 		Content:  fmt.Sprintf("%s 使用了侮辱词汇，消息已被修正，我们不可以向他学习！", ctx.Extra.Author.Nickname),
-		// 		Quote:    ctx.Common.MsgID,
-		// 	},
-		// })
-		// ctx.Session.MessageDelete(ctx.Common.MsgID)
+		// message = strings.ReplaceAll(message, "傻", "*")
+		// message = strings.ReplaceAll(message, "逼", "*")
+		// ctx.Session.MessageUpdate(&khl.MessageUpdate{MessageUpdateBase: khl.MessageUpdateBase{MsgID: ctx.Common.TargetID, Content: message}})
+		ctx.Session.MessageCreate(&khl.MessageCreate{
+			MessageCreateBase: khl.MessageCreateBase{
+				TargetID: ctx.Common.TargetID,
+				Content:  fmt.Sprintf("%s 使用了侮辱词汇，消息已被移除，不可以向他学习哦", ctx.Extra.Author.Nickname),
+				Quote:    ctx.Common.MsgID,
+			},
+		})
+		ctx.Session.MessageDelete(ctx.Common.MsgID)
 	}
 
 }
