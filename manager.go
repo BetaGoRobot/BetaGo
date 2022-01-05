b8ecc66aba766da218df1e2e988ec3d4e46a08c7
KevinMatt <36391318+heyuhengmatt@users.noreply.github.com>
2022-01-05 21:43:14 +0800

    Update manager.go

--- manager.go
+++ manager.go
@@ -50,7 +50,7 @@ func startUpMessage(session *khl.Session) (err error) {
 		MessageCreateBase: khl.MessageCreateBase{
 			Type:     9,
 			TargetID: "7419593543056418",
-			Content:  fmt.Sprintf("---------\n> Robot `%s` is \n`online`\n IP:\t%s\n Time:\t%s---------", robotName, currentIP, currentTime),
+			Content:  fmt.Sprintf("---------\n> Robot `%s` is \n`online`\n IP:\t%s\n Time:\t%s---------", robotName, currentIP, GetCurrentTime()),
 		}})
 	return
 }
@@ -64,7 +64,7 @@ func offlineMessage(session *khl.Session) (err error) {
 		MessageCreateBase: khl.MessageCreateBase{
 			Type:     9,
 			TargetID: "7419593543056418",
-			Content:  fmt.Sprintf("---------\n> Robot `%s` is \n`offline`\n IP:\t%s\n Time:\t%s---------", robotName, currentIP, currentTime),
+			Content:  fmt.Sprintf("---------\n> Robot `%s` is \n`offline`\n IP:\t%s\n Time:\t%s---------", robotName, currentIP, GetCurrentTime()),
 		}})
 	return
 }
