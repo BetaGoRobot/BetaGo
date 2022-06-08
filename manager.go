fa06497fbd4226bbaedbfccba1544a2d2ba81760
Kevin Matt <he_yuheng@163.com>
2022-06-08 21:12:47 +0800

    优化输出

--- manager.go
+++ manager.go
@@ -21,12 +21,12 @@ import (
 var reMatch = regexp.MustCompile(`^(?P<command>\w+)\s+(?P<parameter>.*)$`)
 
 var commandHelper = map[string]string{
-	"help":        "查看帮助 @BetaGo help",
-	"ping":        "检查机器人是否运行正常 @BetaGo ping",
-	"roll":        "掷骰子 @BetaGo roll",
-	"addAdmin":    "添加管理员 @BetaGo addAdmin <userID> <userName>",
-	"removeAdmin": "移除管理员 @BetaGo removeAdmin <userID>",
-	"showAdmin":   "显示所有管理员 @BetaGo showAdmin",
+	"help":        "查看帮助 \n`@BetaGo help`",
+	"ping":        "检查机器人是否运行正常 \n`@BetaGo ping`",
+	"roll":        "掷骰子 \n`@BetaGo roll`",
+	"addAdmin":    "添加管理员 \n`@BetaGo addAdmin <userID> <userName>`",
+	"removeAdmin": "移除管理员 \n`@BetaGo removeAdmin <userID>`",
+	"showAdmin":   "显示所有管理员 \n`@BetaGo showAdmin`",
 }
 
 func debugInterfaceHandler(ctx *khl.KmarkdownMessageContext) {
