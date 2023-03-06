3746ce9bb6c1ca592350bbbbe1e1ece753976550
heyuhengmatt <he_yuheng@163.com>
2023-03-06 23:29:07 +0800

    移除脏话

--- commandHandler/wordcontrol/wordcontrol.go
+++ commandHandler/wordcontrol/wordcontrol.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strings"
 
-	goaway "github.com/TwiN/go-away"
 	"github.com/lonelyevil/kook"
 )
 
@@ -14,11 +13,11 @@ import (
 func RemoveDirtyWords(ctx *kook.KmarkdownMessageContext) {
 	message := ctx.Common.Content
 
-	if strings.Contains(message, "傻") && strings.Contains(message, "逼") || strings.Contains(message, "傻逼") || goaway.IsProfane(message) {
+	if strings.Contains(message, "傻") && strings.Contains(message, "逼") || strings.Contains(message, "傻逼") {
 		ctx.Session.MessageCreate(&kook.MessageCreate{
 			MessageCreateBase: kook.MessageCreateBase{
 				TargetID: ctx.Common.TargetID,
-				Content:  fmt.Sprintf("%s 使用了侮辱发言%s, 消息已被移除, 不可以向他学习哦", ctx.Extra.Author.Nickname, goaway.Censor(message)),
+				Content:  fmt.Sprintf("%s 使用了侮辱发言%s, 消息已被移除, 不可以向他学习哦", ctx.Extra.Author.Nickname, message),
 				Quote:    ctx.Common.MsgID,
 			},
 		})
