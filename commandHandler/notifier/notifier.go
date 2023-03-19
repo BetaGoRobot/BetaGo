01bd518a2d003035754e4b2dc8a87f76257beede
heyuhengmatt <he_yuheng@163.com>
2023-03-20 00:47:26 +0800

    update

--- commandHandler/notifier/notifier.go
+++ commandHandler/notifier/notifier.go
@@ -7,6 +7,7 @@ import (
 	"github.com/BetaGoRobot/BetaGo/betagovar"
 	"github.com/BetaGoRobot/BetaGo/httptool"
 	"github.com/BetaGoRobot/BetaGo/scheduletask"
+	"github.com/BetaGoRobot/BetaGo/utility/gotify"
 	"github.com/enescakir/emoji"
 	"github.com/lonelyevil/kook"
 )
@@ -31,6 +32,17 @@ func StartUpMessage(session *kook.Session) (err error) {
 		return
 	}
 	go func() {
+		title := emoji.DesertIsland.String() + "Online Notifacation" + emoji.Information.String()
+		content := strings.Join(
+			[]string{
+				"Name: \t**", betagovar.RobotName, "**\n",
+				"Time: \t**", time.Now().Add(time.Hour * 8).Local().Format("2006-01-02 15:04:05"), "**\n",
+				"IP: \t**", currentIP, "**\n",
+				"Message: \t**" + betagovar.CommitMessage + "**\n",
+				"Commit-Page: \t[CommitPage](", betagovar.HTMLURL, ")\n",
+				"LeaveYourCommentHere: \t[CommentPage](", betagovar.CommentsURL, ")\n",
+			},
+			"")
 		cardMessage, _ := kook.CardMessage{
 			&kook.CardMessageCard{
 				Theme: kook.CardThemeInfo,
@@ -38,27 +50,19 @@ func StartUpMessage(session *kook.Session) (err error) {
 				Modules: []interface{}{
 					kook.CardMessageHeader{
 						Text: kook.CardMessageElementText{
-							Content: emoji.DesertIsland.String() + "Online Notifacation" + emoji.Information.String(),
+							Content: title,
 							Emoji:   false,
 						},
 					},
 					kook.CardMessageSection{
 						Text: kook.CardMessageElementKMarkdown{
-							Content: strings.Join(
-								[]string{
-									"Name: \t**", betagovar.RobotName, "**\n",
-									"Time: \t**", time.Now().Add(time.Hour * 8).Local().Format("2006-01-02 15:04:05"), "**\n",
-									"IP: \t**", currentIP, "**\n",
-									"Message: \t**" + betagovar.CommitMessage + "**\n",
-									"Commit-Page: \t[CommitPage](", betagovar.HTMLURL, ")\n",
-									"LeaveYourCommentHere: \t[CommentPage](", betagovar.CommentsURL, ")\n",
-								},
-								""),
+							Content: content,
 						},
 					},
 				},
 			},
 		}.BuildMessage()
+		gotify.SendMessage(title, content, 5)
 		session.MessageCreate(
 			&kook.MessageCreate{
 				MessageCreateBase: kook.MessageCreateBase{
@@ -120,6 +124,16 @@ func OfflineMessage(session *kook.Session) (err error) {
 	if err != nil {
 		return
 	}
+	title := emoji.DesertIsland.String() + "Offline Notifacation" + emoji.Information.String()
+	content := strings.Join([]string{
+		"Name: \t**", betagovar.RobotName, "**\n",
+		"Time: \t**", time.Now().Add(time.Hour * 8).Format("2006-01-02 15:04:05"), "**\n",
+		"IP: \t**", currentIP, "**\n",
+		"Message: \t**", betagovar.CommitMessage, "**\n",
+		"Commit-Page: \t[CommitPage](", betagovar.HTMLURL, ")\n",
+		"LeaveYourCommentHere: \t[CommentPage](", betagovar.CommentsURL, ")\n",
+	},
+		"")
 	cardMessage, err := kook.CardMessage{
 		&kook.CardMessageCard{
 			Theme: "info",
@@ -127,26 +141,19 @@ func OfflineMessage(session *kook.Session) (err error) {
 			Modules: []interface{}{
 				kook.CardMessageHeader{
 					Text: kook.CardMessageElementText{
-						Content: emoji.DesertIsland.String() + "Offline Notifacation" + emoji.Information.String(),
+						Content: title,
 						Emoji:   false,
 					},
 				},
 				kook.CardMessageSection{
 					Text: kook.CardMessageElementKMarkdown{
-						Content: strings.Join([]string{
-							"Name: \t**", betagovar.RobotName, "**\n",
-							"Time: \t**", time.Now().Add(time.Hour * 8).Format("2006-01-02 15:04:05"), "**\n",
-							"IP: \t**", currentIP, "**\n",
-							"Message: \t**", betagovar.CommitMessage, "**\n",
-							"Commit-Page: \t[CommitPage](", betagovar.HTMLURL, ")\n",
-							"LeaveYourCommentHere: \t[CommentPage](", betagovar.CommentsURL, ")\n",
-						},
-							""),
+						Content: content,
 					},
 				},
 			},
 		},
 	}.BuildMessage()
+	gotify.SendMessage(title, content, 5)
 	session.MessageCreate(
 		&kook.MessageCreate{
 			MessageCreateBase: kook.MessageCreateBase{
