90bda61bb2142927879eb00f19b8e9b9d02d889d
kevinmatthe <he_yuheng@163.com>
2025-01-21 13:16:17 +0800

    update

--- handler/larkhandler/message/repeat_op.go
+++ handler/larkhandler/message/repeat_op.go
@@ -2,6 +2,7 @@ package message
 
 import (
 	"context"
+	"sort"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -100,10 +101,14 @@ func (r *RepeatMsgOperator) Run(ctx context.Context, event *larkim.P2MessageRece
 		textMsgBuilder := larkim.NewTextMsgBuilder()
 
 		// rebuild at msg
-		subMsgList := strings.Split(msg, " ")
+		subStrings := []string{}
+		for _, mention := range event.Event.Message.Mentions {
+			subStrings = append(subStrings, *mention.Key)
+		}
+		subMsgList := RebuildAtMsg(msg, subStrings)
 		mentionsMap := make(map[string]*larkim.MentionEvent)
 		for _, mention := range event.Event.Message.Mentions {
-			mentionsMap[*mention.Key] = mention
+			strings.Index(msg, *mention.Key)
 		}
 		for index, subMsg := range subMsgList {
 			if mentionEvent, ok := mentionsMap[subMsg]; ok {
@@ -127,4 +132,52 @@ func (r *RepeatMsgOperator) Run(ctx context.Context, event *larkim.P2MessageRece
 		}
 	}
 	return
+}
+
+func RebuildAtMsg(input string, substrings []string) []string {
+	result := []string{}
+	start := 0
+
+	// Keep track of the positions to split
+	splitPositions := []int{}
+
+	// Iterate through the input to find all occurrences of substrings
+	for _, sub := range substrings {
+		start = 0
+		for {
+			pos := strings.Index(input[start:], sub)
+			if pos == -1 {
+				break
+			}
+			actualPos := start + pos
+			splitPositions = append(splitPositions, actualPos, actualPos+len(sub))
+			start = actualPos + len(sub)
+		}
+	}
+
+	// Sort the positions to split
+	sort.Slice(splitPositions, func(i, j int) bool { return splitPositions[i] < splitPositions[j] })
+
+	// Remove duplicate positions
+	uniquePositions := []int{}
+	for i, pos := range splitPositions {
+		if i == 0 || pos != splitPositions[i-1] {
+			uniquePositions = append(uniquePositions, pos)
+		}
+	}
+
+	// Add start and end of the string to the positions if not already present
+	if uniquePositions[0] != 0 {
+		uniquePositions = append([]int{0}, uniquePositions...)
+	}
+	if uniquePositions[len(uniquePositions)-1] != len(input) {
+		uniquePositions = append(uniquePositions, len(input))
+	}
+
+	// Extract substrings based on split positions
+	for i := 0; i < len(uniquePositions)-1; i++ {
+		result = append(result, input[uniquePositions[i]:uniquePositions[i+1]])
+	}
+
+	return result
 }
