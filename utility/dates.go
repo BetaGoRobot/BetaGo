c4831ebb7694f04475a60c4176f33f51e53b51ac
kevinmatthe <he_yuheng@163.com>
2025-04-27 17:40:50 +0800

    🐛 fix(dates): 修复时间戳转换时区问题
    - 在时间戳转换时添加UTC()方法，确保返回的时间字符串为UTC时间

--- utility/dates.go
+++ utility/dates.go
@@ -7,5 +7,5 @@ import (
 
 func EpoMil2DateStr(epoMil string) string {
 	epoMilInt, _ := strconv.ParseInt(epoMil, 10, 64)
-	return time.Unix(int64(epoMilInt)/1000, 0).Format("2006-01-02 15:04:05")
+	return time.Unix(int64(epoMilInt)/1000, 0).UTC().Format("2006-01-02 15:04:05")
 }
