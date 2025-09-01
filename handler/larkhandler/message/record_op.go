efe321862c4d0527ba67118ed45094e8cb7ade36
kevinmatthe <he_yuheng@163.com>
2025-09-01 15:36:27 +0800

    ✨ feat(record_op): 添加表情包资源存储功能
    - 新增对飞书表情包消息的处理逻辑
    - 使用gorm的OnConflict实现重复插入保护
    - 将表情包file_key存储到数据库
    - 添加相关依赖导入

--- handler/larkhandler/message/record_op.go
+++ handler/larkhandler/message/record_op.go
@@ -3,11 +3,17 @@ package message
 import (
 	"context"
 
+	"github.com/BetaGoRobot/BetaGo/consts"
 	handlerbase "github.com/BetaGoRobot/BetaGo/handler/handler_base"
+	"github.com/BetaGoRobot/BetaGo/utility/database"
 	"github.com/BetaGoRobot/BetaGo/utility/larkutils/larkimg"
+	"github.com/BetaGoRobot/BetaGo/utility/log"
 	"github.com/BetaGoRobot/BetaGo/utility/otel"
 	"github.com/BetaGoRobot/go_utils/reflecting"
+	"github.com/bytedance/sonic"
+	"github.com/kevinmatthe/zaplog"
 	larkim "github.com/larksuite/oapi-sdk-go/v3/service/im/v1"
+	"gorm.io/gorm/clause"
 )
 
 var _ Op = &RepeatMsgOperator{}
@@ -64,5 +70,23 @@ func (r *RecordMsgOperator) Run(ctx context.Context, event *larkim.P2MessageRece
 			}
 		}
 	}
+	msg := event.Event.Message
+	if msg != nil && *msg.MessageType == larkim.MsgTypeSticker {
+		contentMap := make(map[string]string)
+		err := sonic.UnmarshalString(*msg.Content, &contentMap)
+		if err != nil {
+			log.Zlog.Error("repeatMessage", zaplog.Error(err))
+			return err
+		}
+		stickerKey := contentMap["file_key"]
+		// 表情包为全局file_key，可以直接存下
+		if result := database.GetDbConnection().Clauses(clause.OnConflict{DoNothing: true}).
+			Create(&database.ReactImageMeterial{GuildID: *msg.ChatId, FileID: stickerKey, Type: consts.LarkResourceTypeSticker}); result.Error != nil {
+			return result.Error
+		} else if result.RowsAffected == 0 {
+			return nil
+		}
+	}
+
 	return
 }
