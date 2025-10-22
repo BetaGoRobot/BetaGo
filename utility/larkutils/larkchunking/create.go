57e9b6cca2d543dacb7e17d037e081aca0cccbb6
kevinmatthe <he_yuheng@163.com>
2025-10-22 15:34:50 +0800

    ✨ feat(larkchunking): 新增通用消息构建函数并重构相关代码
    - 新增common.go文件，包含buildLineCommon函数用于处理消息构建通用逻辑
    - 重构create.go和reply.go文件，移除重复代码并调用buildLineCommon函数
    - 优化代码结构，提高可维护性和复用性

--- utility/larkutils/larkchunking/create.go
+++ utility/larkutils/larkchunking/create.go
@@ -2,17 +2,9 @@ package larkchunking
 
 import (
 	"context"
-	"fmt"
 	"strconv"
-	"strings"
 	"time"
 
-	"github.com/BetaGoRobot/BetaGo/utility"
-	"github.com/BetaGoRobot/BetaGo/utility/larkutils/grouputil"
-	"github.com/BetaGoRobot/BetaGo/utility/larkutils/larkconsts"
-	"github.com/BetaGoRobot/BetaGo/utility/larkutils/larkmsgutils"
-	"github.com/BetaGoRobot/BetaGo/utility/log"
-	"github.com/bytedance/sonic"
 	"github.com/kevinmatthe/zaplog"
 	larkim "github.com/larksuite/oapi-sdk-go/v3/service/im/v1"
 )
@@ -39,57 +31,13 @@ func (m *LarkMessageRespCreate) TimeStamp() (res int64) {
 }
 
 func (m *LarkMessageRespCreate) BuildLine() (line string) {
-	mentions := m.Data.Mentions
-
-	tmpList := make([]string, 0)
-	for msgItem := range larkmsgutils.
-		GetContentItemsSeq(
-			&larkim.EventMessage{
-				Content:     m.Data.Body.Content,
-				MessageType: m.Data.MsgType,
-			},
-		) {
-		switch msgItem.Tag {
-		case "at", "text":
-			if msgItem.Tag == "text" {
-				m := map[string]string{}
-				if err := sonic.UnmarshalString(msgItem.Content, &m); err == nil {
-					msgItem.Content = m["text"]
-				}
-			}
-			if len(mentions) > 0 {
-				for _, mention := range mentions {
-					if mention.Key != nil {
-						if strings.HasPrefix(*mention.Name, "不太正经的网易云音乐机器人") {
-							*mention.Name = "你"
-						}
-						msgItem.Content = strings.ReplaceAll(msgItem.Content, *mention.Key, fmt.Sprintf("@%s", *mention.Name))
-					}
-				}
-			}
-			fallthrough
-		default:
-			content := strings.ReplaceAll(msgItem.Content, "\n", "<换行>")
-			if strings.TrimSpace(content) != "" {
-				tmpList = append(tmpList, content)
-			}
-		}
-	}
-	userName := ""
-	if *m.Data.Sender.Id != larkconsts.BotAppID {
-		userName = "机器人"
-	} else {
-		member, err := grouputil.GetUserMemberFromChat(context.Background(), *m.Data.ChatId, *m.Data.Sender.Id)
-		if err != nil {
-			log.Zlog.Error("got error openID", zaplog.String("openID", *m.Data.Sender.Id))
-		}
-		if member == nil {
-			userName = "NULL"
-		} else {
-			userName = *member.Name
-		}
-	}
-
-	createTime := time.UnixMilli(m.TimeStamp()).In(utility.UTCPlus8Loc()).Format(time.DateTime)
-	return fmt.Sprintf("[%s](%s) <%s>: %s", createTime, *m.Data.Sender.Id, userName, strings.Join(tmpList, ";"))
+	return buildLineCommon(
+		context.Background(),
+		m.Data.Body.Content,
+		m.Data.MsgType,
+		m.Data.Mentions,
+		m.Data.Sender.Id,
+		m.Data.ChatId,
+		m.TimeStamp(),
+	)
 }
