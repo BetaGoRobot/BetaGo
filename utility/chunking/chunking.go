dd87b23b4c9dba8f227ffca94018bd8a492f9465
kevinmatthe <he_yuheng@163.com>
2025-10-24 16:32:45 +0800

    🐛 fix(chunking): 调整会话非活跃超时时间
    - 将INACTIVITY_TIMEOUT从1分钟增加到3分钟
    - 优化长时间会话的处理性能

--- utility/chunking/chunking.go
+++ utility/chunking/chunking.go
@@ -31,7 +31,7 @@ import (
 const (
 	// INACTIVITY_TIMEOUT 定义会话非活跃超时时间
 	// INACTIVITY_TIMEOUT = 30 * time.Second
-	INACTIVITY_TIMEOUT = 1 * time.Minute
+	INACTIVITY_TIMEOUT = 3 * time.Minute
 	// MAX_CHUNK_SIZE 定义在强制合并前一个块中的最大消息数
 	MAX_CHUNK_SIZE = 50
 )
