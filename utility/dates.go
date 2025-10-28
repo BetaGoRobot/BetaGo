33ebd9ae4c20b24029fa267e2747c2f08d82cefd
kevinmatthe <he_yuheng@163.com>
2025-10-28 14:26:09 +0800

    🐛 fix(dates): 优化时区处理逻辑
    - 使用UTCPlus8Loc()替代手动添加8小时
    - 提高代码可读性和维护性
    - 确保时区转换逻辑一致

--- utility/dates.go
+++ utility/dates.go
@@ -7,10 +7,10 @@ import (
 
 func EpoMil2DateStr(epoMil string) string {
 	epoMilInt, _ := strconv.ParseInt(epoMil, 10, 64)
-	return time.Unix(int64(epoMilInt)/1000, 0).UTC().Add(time.Hour * 8).Format("2006-01-02 15:04:05")
+	return time.Unix(int64(epoMilInt)/1000, 0).In(UTCPlus8Loc()).Format("2006-01-02 15:04:05")
 }
 
 func EpoMicro2DateStr(epoMicro string) string {
 	epoMilInt, _ := strconv.ParseInt(epoMicro, 10, 64)
-	return time.Unix(int64(epoMilInt)/1000/1000, 0).UTC().Add(time.Hour * 8).Format("2006-01-02 15:04:05")
+	return time.Unix(int64(epoMilInt)/1000/1000, 0).In(UTCPlus8Loc()).Format("2006-01-02 15:04:05")
 }
